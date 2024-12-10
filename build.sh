6dacd9a0e195b36050ec60a18c2de8376924f2d0
acostadon <dacosta@nvidia.com>
2024-12-10 15:43:55 -0500

    get xml from cloud instead of test area

--- build.sh
+++ build.sh
@@ -102,13 +102,8 @@ if hasArg docs || buildDefault; then
         mkdir -p "${XML_DIR}"
         export XML_DIR_${PROJECT^^}="$XML_DIR"
 
-        if [[ $PROJECT == "libcugraph" ]]; then
-            echo "TMP (FIXME) downloading xml for ${PROJECT} into ${XML_DIR}. Environment variable XML_DIR_${PROJECT^^} is set to ${XML_DIR}"
-            curl -O "https://raw.githubusercontent.com/BradReesWork/data/main/xml.tar.gz"
-        else
-            echo "downloading xml for ${PROJECT} into ${XML_DIR}. Environment variable XML_DIR_${PROJECT^^} is set to ${XML_DIR}"
-            curl -O "https://d1664dvumjb44w.cloudfront.net/${PROJECT}/xml_tar/${RAPIDS_VERSION}/xml.tar.gz"
-        fi
+        echo "downloading xml for ${PROJECT} into ${XML_DIR}. Environment variable XML_DIR_${PROJECT^^} is set to ${XML_DIR}"
+        curl -O "https://d1664dvumjb44w.cloudfront.net/${PROJECT}/xml_tar/${RAPIDS_VERSION}/xml.tar.gz"
 
         tar -xzf xml.tar.gz -C "${XML_DIR}"
         rm "./xml.tar.gz"
