2203a9390e77ebfce689171eb494cfdb0fd8aed2
James Lamb <jlamb@nvidia.com>
2024-12-18 11:17:24 -0600

    add docs-building CI (#46)
    Replaces #44.
    
    Trying a new PR that enables docs builds on PRs and branch builds.
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
      - Brad Rees (https://github.com/BradReesWork)
      - Jake Awe (https://github.com/AyodeAwe)
    
    Approvers:
      - Don Acosta (https://github.com/acostadon)
      - Mike Sarahan (https://github.com/msarahan)
    
    URL: https://github.com/rapidsai/cugraph-docs/pull/46

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -6,6 +6,9 @@ set -euo pipefail
 rapids-logger "Create test conda environment"
 . /opt/conda/etc/profile.d/conda.sh
 
+RAPIDS_VERSION="$(rapids-version)"
+RAPIDS_VERSION_MAJOR_MINOR="$(rapids-version-major-minor)"
+
 rapids-dependency-file-generator \
   --output conda \
   --file-key docs \
@@ -14,42 +17,31 @@ rapids-dependency-file-generator \
 rapids-mamba-retry env create --yes -f env.yaml -n docs
 conda activate docs
 
+export RAPIDS_DOCS_DIR="$(mktemp -d)"
+
 rapids-print-env
 
-rapids-logger "Downloading artifacts from previous jobs"
-CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
-PYTHON_CHANNEL=$(rapids-download-conda-from-s3 python)
-
 if [[ "${RAPIDS_CUDA_VERSION}" == "11.8.0" ]]; then
-  CONDA_CUDA_VERSION="11.8"
   DGL_CHANNEL="dglteam/label/cu118"
 else
-  CONDA_CUDA_VERSION="12.1"
   DGL_CHANNEL="dglteam/label/cu121"
 fi
 
 rapids-mamba-retry install \
-  --channel "${CPP_CHANNEL}" \
-  --channel "${PYTHON_CHANNEL}" \
   --channel conda-forge \
   --channel pyg \
   --channel nvidia \
   --channel "${DGL_CHANNEL}" \
-  libcugraph \
-  pylibcugraph \
-  cugraph \
-  cugraph-pyg \
-  cugraph-dgl \
-  libcugraph_etl \
-  pylibcugraphops \
-  pylibwholegraph \
+  "libcugraph==${RAPIDS_VERSION}.*" \
+  "pylibcugraph=${RAPIDS_VERSION}.*" \
+  "cugraph=${RAPIDS_VERSION}.*" \
+  "cugraph-pyg=${RAPIDS_VERSION}.*" \
+  "cugraph-dgl=${RAPIDS_VERSION}.*" \
+  "libcugraph_etl=${RAPIDS_VERSION}.*" \
+  "pylibcugraphops=${RAPIDS_VERSION}.*" \
+  "pylibwholegraph=${RAPIDS_VERSION}.*" \
   pytorch \
-  "cuda-version=${CONDA_CUDA_VERSION}"
-
-export RAPIDS_VERSION="$(rapids-version)"
-export RAPIDS_VERSION_MAJOR_MINOR="$(rapids-version-major-minor)"
-export RAPIDS_VERSION_NUMBER="$RAPIDS_VERSION_MAJOR_MINOR"
-export RAPIDS_DOCS_DIR="$(mktemp -d)"
+  "cuda-version=${RAPIDS_CUDA_VERSION%.*}"
 
 PROJ_LIST=("libcugraph libcugraphops libwholegraph")
 
@@ -57,13 +49,7 @@ for PROJECT in ${PROJ_LIST}; do
   rapids-logger "Download ${PROJECT} xml_tar"
   TMP_DIR=$(mktemp -d)
   export XML_DIR_${PROJECT^^}="$TMP_DIR"
-
-  if [[ $PROJECT == "libcugraph" ]]; then
-      echo "TMP (FIXME) downloading xml for ${PROJECT} into ${XML_DIR}. Environment variable XML_DIR_${PROJECT^^} is set to ${XML_DIR}"
-      curl -O "https://raw.githubusercontent.com/BradReesWork/data/main/xml.tar.gz"
-  else
-    curl "https://d1664dvumjb44w.cloudfront.net/${PROJECT}/xml_tar/${RAPIDS_VERSION_NUMBER}/xml.tar.gz" | tar -xzf - -C "${TMP_DIR}"
-  fi
+  curl "https://d1664dvumjb44w.cloudfront.net/${PROJECT}/xml_tar/${RAPIDS_VERSION_MAJOR_MINOR}/xml.tar.gz" | tar -xzf - -C "${TMP_DIR}"
 done
 
 
@@ -77,4 +63,4 @@ mkdir -p "${RAPIDS_DOCS_DIR}/cugraph-docs/html"
 mv _html/* "${RAPIDS_DOCS_DIR}/cugraph-docs/html"
 popd
 
-rapids-upload-docs
+RAPIDS_VERSION_NUMBER="${RAPIDS_VERSION_MAJOR_MINOR}" rapids-upload-docs
