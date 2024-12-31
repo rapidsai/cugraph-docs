793c2ec5733956a073fb5568cc4a5cbd13657565
James Lamb <jlamb@nvidia.com>
2024-12-31 11:56:02 -0600

    simplify building and testing configs

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -6,7 +6,6 @@ set -euo pipefail
 rapids-logger "Create test conda environment"
 . /opt/conda/etc/profile.d/conda.sh
 
-RAPIDS_VERSION="$(rapids-version)"
 RAPIDS_VERSION_MAJOR_MINOR="$(rapids-version-major-minor)"
 
 rapids-dependency-file-generator \
@@ -21,35 +20,12 @@ export RAPIDS_DOCS_DIR="$(mktemp -d)"
 
 rapids-print-env
 
-if [[ "${RAPIDS_CUDA_VERSION}" == "11.8.0" ]]; then
-  DGL_CHANNEL="dglteam/label/cu118"
-else
-  DGL_CHANNEL="dglteam/label/cu121"
-fi
-
-rapids-mamba-retry install \
-  --channel conda-forge \
-  --channel pyg \
-  --channel nvidia \
-  --channel "${DGL_CHANNEL}" \
-  "libcugraph==${RAPIDS_VERSION}.*" \
-  "pylibcugraph=${RAPIDS_VERSION}.*" \
-  "cugraph=${RAPIDS_VERSION}.*" \
-  "cugraph-pyg=${RAPIDS_VERSION}.*" \
-  "cugraph-dgl=${RAPIDS_VERSION}.*" \
-  "libcugraph_etl=${RAPIDS_VERSION}.*" \
-  "pylibcugraphops=${RAPIDS_VERSION}.*" \
-  "pylibwholegraph=${RAPIDS_VERSION}.*" \
-  pytorch \
-  "cuda-version=${RAPIDS_CUDA_VERSION%.*}"
-
 PROJ_LIST=("libcugraph libcugraphops libwholegraph")
 
 for PROJECT in ${PROJ_LIST}; do
   rapids-logger "Download ${PROJECT} xml_tar"
   TMP_DIR=$(mktemp -d)
   export XML_DIR_${PROJECT^^}="$TMP_DIR"
-
   curl "https://d1664dvumjb44w.cloudfront.net/${PROJECT}/xml_tar/${RAPIDS_VERSION_MAJOR_MINOR}/xml.tar.gz" | tar -xzf - -C "${TMP_DIR}"
 done
 
@@ -60,8 +36,8 @@ pushd docs/cugraph-docs
 # type of failure well.
 python -c "import cugraph; print(f'Using cugraph: {cugraph}')"
 sphinx-build -b dirhtml source _html
-mkdir -p "${RAPIDS_DOCS_DIR}/cugraph/html"
-mv _html/* "${RAPIDS_DOCS_DIR}/cugraph/html"
+mkdir -p "${RAPIDS_DOCS_DIR}/cugraph-docs/html"
+mv _html/* "${RAPIDS_DOCS_DIR}/cugraph-docs/html"
 popd
 
 RAPIDS_VERSION_NUMBER="${RAPIDS_VERSION_MAJOR_MINOR}" rapids-upload-docs
