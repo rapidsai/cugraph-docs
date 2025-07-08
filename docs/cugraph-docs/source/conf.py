db2636332c8081fdfb57cbab9455cb47f28faca4
acostadon <dacosta@nvidia.com>
2025-07-08 10:46:21 -0400

    removed a sphinx contrib extension which was cauding rendering conflicts

--- docs/cugraph-docs/source/conf.py
+++ docs/cugraph-docs/source/conf.py
@@ -44,7 +44,6 @@ extensions = [
     "breathe",
     "myst_parser",
     "sphinx.ext.intersphinx",
-    "sphinxcontrib.jsmath",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "numpydoc",
@@ -57,6 +56,11 @@ extensions = [
     "nbsphinx",
     "sphinx_copybutton",
 ]
+
+myst_enable_extensions = [
+    "dollarmath",
+]
+myst_dmath_double_inline = True
 
 
 jsmath_path = "https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS_HTML"
